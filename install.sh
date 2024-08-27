a0522a4f363db1d0f7c5d64569fd0d0d6565c39e
mateusmoutinho <mateusmoutinho01@gmail.com>
2024-08-26 22:13:53 -0300

    att

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 
-CWEBSTUDIO_VERSION ="v2.000"
+CWEBSTUDIO_VERSION="v2.000"
 
 if [[ $EUID -ne 0 ]]; then
    echo "these script must be executed as root"
@@ -7,16 +7,22 @@ if [[ $EUID -ne 0 ]]; then
 fi
 
 # in case of re-run these script
-rm -rf alpine
+
+rn alpine.tar.gz
 rm -rf src/dependencies
 mkdir src/dependencies
 
+umount ./alpine/dev
+rm -rf alpine
 
-wget https://github.com/OUIsolutions/CWebStudio/releases/download/$CWEBSTUDIO_VERSION/CWebStudio.h  src/dependencies/CWebStudio.h
 
-wget http://dl-cdn.alpinelinux.org/alpine/v3.12/releases/x86_64/alpine-minirootfs-3.12.0-x86_64.tar.gz
+wget https://github.com/OUIsolutions/CWebStudio/releases/download/$CWEBSTUDIO_VERSION/CWebStudio.h -O src/dependencies/CWebStudio.h
+
+
+wget http://dl-cdn.alpinelinux.org/alpine/v3.12/releases/x86_64/alpine-minirootfs-3.12.0-x86_64.tar.gz -O alpine.tar.gz
 mkdir alpine
-tar zxf alpine-minirootfs-*.tar.gz -C ./alpine
+tar zxf alpine.tar.gz -C ./alpine
+
 mount /dev/ ./alpine/dev/ --bind
 mount -o remount,ro,bind ./alpine/dev
 cp -L /etc/resolv.conf ./alpine/etc/
@@ -25,4 +31,5 @@ chroot ./alpine apk add gcc
 chroot ./alpine apk add musl-dev
 umount ./alpine/dev
 rm -f /etc/resolve.conf
+rm alpine.tar.gz
 echo "alpine installed with gcc"
