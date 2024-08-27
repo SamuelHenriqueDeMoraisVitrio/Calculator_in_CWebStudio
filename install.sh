5f1ef0e8b1d1dde304590d7b4ff798adb22e3013
SamuelHenriqueDeMoraisVitrio <samuelhenriquemoraisvitrio@gmail.com>
2024-08-26 23:58:18 -0300

    tt

--- install.sh
+++ install.sh
@@ -1,33 +1,80 @@
-
+# Definir variáveis
+URL="http://cdimage.ubuntu.com/ubuntu-base/releases/20.04/release/ubuntu-base-20.04.3-base-amd64.tar.gz"
+DIRETORIO="container"
+TAR_FILE="ubuntu-base.tar.gz"
 CWEBSTUDIO_VERSION="v2.000"
 
+# Verificar se o script está sendo executado como root
 if [[ $EUID -ne 0 ]]; then
-   echo "these script must be executed as root"
+   echo "Este script deve ser executado como root"
    exit 1
 fi
 
-# in case of re-run these script
-
-rn alpine.tar.gz
+echo ""
+echo "delet dependencies"
 rm -rf src/dependencies
 mkdir src/dependencies
+echo ""
+echo "Install dependencies"
+curl -L https://github.com/OUIsolutions/CWebStudio/releases/download/$CWEBSTUDIO_VERSION/CWebStudio.h -o src/dependencies/CWebStudio.h
+echo ""
+echo ""
+echo ""
+#resouvendo problemas de montagem anterior
+umount "$DIRETORIO/proc"
+umount "$DIRETORIO/sys"
+umount "$DIRETORIO/dev/pts"
+umount "$DIRETORIO/dev"
+umount "$DIRETORIO"
 
-umount ./alpine/dev
-rm -rf alpine
+
+#!/bin/bash
+echo "deletando container antigo"
+sudo rm -rf $DIRETORIO
+
+echo "Baixando o sistema de arquivos mínimo do Ubuntu..."
+wget -O "$TAR_FILE" "$URL"
+
+# Verificar se o download foi bem-sucedido
+if [ $? -ne 0 ]; then
+    echo "Falha ao baixar o arquivo"
+    exit 1
+fi
+
+# Criar o diretório onde o sistema de arquivos será montado
+echo "Criando diretório $DIRETORIO..."
+mkdir -p "$DIRETORIO"
+
+# Descompactar o sistema de arquivos
+echo "Descompactando o sistema de arquivos..."
+tar -xzf "$TAR_FILE" -C "$DIRETORIO"
+
+#da acesso a internet
+ cp -L /etc/resolv.conf ./$DIRETORIO/etc/
 
 
-wget https://github.com/OUIsolutions/CWebStudio/releases/download/$CWEBSTUDIO_VERSION/CWebStudio.h -O src/dependencies/CWebStudio.h
-wget https://dl-cdn.alpinelinux.org/alpine/v3.20/releases/x86_64/alpine-minirootfs-3.20.2-x86_64.tar.gz -O alpine.tar.gz
-mkdir alpine
-tar zxf alpine.tar.gz -C ./alpine
+# Montar os sistemas de arquivos necessários para o chroot
+echo "Montando sistemas de arquivos necessários..."
+mount --bind /proc "$DIRETORIO/proc"
+mount --bind /sys "$DIRETORIO/sys"
+mount --bind /dev "$DIRETORIO/dev"
+mount --bind /dev/pts "$DIRETORIO/dev/pts"
 
-mount /dev/ ./alpine/dev/ --bind
-mount -o remount,ro,bind ./alpine/dev
-cp -L /etc/resolv.conf ./alpine/etc/
-mkdir -p /alpine/root
-chroot ./alpine apk add gcc
-chroot ./alpine apk add musl-dev
-umount ./alpine/dev
 
-rm alpine.tar.gz
-echo "alpine installed with gcc"
+echo "Instalando o projeto"
+chroot $DIRETORIO /bin/bash -c 'apt update'
+chroot $DIRETORIO /bin/bash -c 'DEBIAN_FRONTEND=noninteractive apt install -y gcc build-essential'
+
+
+
+# Após sair do chroot, desmontar os sistemas de arquivos
+echo "Desmontando sistemas de arquivos..."
+umount "$DIRETORIO/proc"
+umount "$DIRETORIO/sys"
+umount "$DIRETORIO/dev/pts"
+umount "$DIRETORIO/dev"
+umount "$DIRETORIO"
+
+sudo rm  $DIRETORIO/etc/resolv.conf
+
+echo "Processo concluído."
--- run.sh
+++ run.sh
@@ -1,16 +1,55 @@
 
+
+
+# Definir variáveis
+URL="http://cdimage.ubuntu.com/ubuntu-base/releases/20.04/release/ubuntu-base-20.04.3-base-amd64.tar.gz"
+DIRETORIO="container"
+PROJECTS="projects"
+TAR_FILE="ubuntu-base.tar.gz"
+COMANDO="cd $PROJECTS && gcc src/main.c && ./a.out"
+
+#resouvendo problemas de montagem anterior
+umount "$DIRETORIO/proc"
+umount "$DIRETORIO/sys"
+umount "$DIRETORIO/dev/pts"
+umount "$DIRETORIO/dev"
+umount "$DIRETORIO"
+
+
 # Verificar se o script está sendo executado como root
 if [[ $EUID -ne 0 ]]; then
-   echo "these script must be executed as root"
+   echo "Este script deve ser executado como root"
    exit 1
 fi
 
-mount /dev/ ./alpine/dev/ --bind
-mount -o remount,ro,bind ./alpine/dev
-cp -L /etc/resolv.conf ./alpine/etc/
-cp -r src alpine/src
-cp -r static alpine/static
-chroot ./alpine gcc src/main.c -o hellokitty.out
-chroot ./alpine  ./hellokitty.out
-umount ./alpine/dev
-echo "container unmounted"
+#da acesso a internet (nescessário se seu app vai usar internet emquanto roda)
+sudo cp -L /etc/resolv.conf ./$DIRETORIO/etc/
+sudo mkdir ./$DIRETORIO/$PROJECTS
+sudo cp -r ./src ./static ./$DIRETORIO/$PROJECTS
+
+
+# Montar os sistemas de arquivos necessários para o chroot
+echo "Montando sistemas de arquivos necessários..."
+mount --bind /proc "$DIRETORIO/proc"
+mount --bind /sys "$DIRETORIO/sys"
+mount --bind /dev "$DIRETORIO/dev"
+mount --bind /dev/pts "$DIRETORIO/dev/pts"
+
+xhost +local:
+
+#starta o app
+chroot $DIRETORIO /bin/bash -c  "$COMANDO"
+
+
+# Após sair do chroot, desmontar os sistemas de arquivos
+echo "Desmontando sistemas de arquivos..."
+umount "$DIRETORIO/proc"
+umount "$DIRETORIO/sys"
+umount "$DIRETORIO/dev/pts"
+umount "$DIRETORIO/dev"
+umount "$DIRETORIO"
+
+#Remove o arquivo de interent do container
+sudo rm  $DIRETORIO/etc/resolv.conf
+
+echo "Processo concluído."
